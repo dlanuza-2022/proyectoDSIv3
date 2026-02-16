5e92e0e3a29aa13e04270ba983d39b3e3e5dfe7b
Dorian <dorian.lanuza2022@gmail.com>
2026-02-16 10:53:09 -0600

    Se agregaron las importaciones necesarias en auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { AuthService } from './auth.service';
 
