56506116dba08fe39ac6ac648fa7823769d67735
Dorian <dorian.lanuza2022@gmail.com>
2026-02-16 10:51:50 -0600

    Se agregó PrismaModule en auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   imports: [
+    PrismaModule,
     JwtModule.register({
       secret: 'AZXY8899*',
       signOptions: { expiresIn: '1h' },
