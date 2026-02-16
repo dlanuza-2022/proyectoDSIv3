01724400e2237012921194c22b934477d19b365f
Dorian <dorian.lanuza2022@gmail.com>
2026-02-16 09:28:26 -0600

    Creacion de login.dto.ts

---
+++ src/auth/dto/login.dto.ts
@@ -0,0 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+
+export class loginDto {
+    @ApiProperty({required: true})
+    email: string;
+
+    @ApiProperty({required: true})
+    password: string;
+}
