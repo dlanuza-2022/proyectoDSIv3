f78b90dd2b9c0c2e144ceec0dcc703c162bf3df0
Dorian <dorian.lanuza2022@gmail.com>
2026-02-16 09:57:58 -0600

    Modificiacion de archivo auth.controller.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,12 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -2,9 +2,9 @@ import { ApiProperty } from "@nestjs/swagger";
 
 
 export class LoginDto {
-    @ApiProperty({required: true})
-    email: string;
+  @ApiProperty({ required: true })
+  email: string;
 
-    @ApiProperty({required: true})
-    password: string;
+  @ApiProperty({ required: true })
+  password: string;
 }
